d3dfbc064371b2ec30ae62b0251923712720f4c7
Johnny Choo <jchoo156@gmail.com>
2017-01-15 15:29:52 -0800

    refactor all items component

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,9 +1,17 @@
 var Body = React.createClass({
+  getInitialState() {
+    return { items: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response })})
+  },
+
   render() {
     return (
       <div>
         <NewItem />
-        <AllItems />
+        <AllItems items={this.state.items} />
       </div>
     )
   }
--- app/assets/javascripts/components/_new_items.js.jsx
+++ app/assets/javascripts/components/_new_items.js.jsx
@@ -2,8 +2,14 @@ var NewItem = React.createClass({
   handleClick() {
     var name = this.refs.name.value;
     var description = this.refs.description.value;
-
-    console.log('The name value is ' + name + ' the description value is ' + description);
+    $.ajax({
+      url: '/api/v1/items',
+      type: 'POST',
+      data: { item: { name: name, description: description }},
+      success: (response) => {
+        console.log('it worked', response);
+      }
+    });
   },
 
   render() {

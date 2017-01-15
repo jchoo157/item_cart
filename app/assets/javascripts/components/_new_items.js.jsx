5dd911163c86641342defa2ca92b132cb270e2f1
Johnny Choo <jchoo156@gmail.com>
2017-01-15 15:02:46 -0800

    re-generate site controller

--- app/assets/javascripts/components/_new_items.js.jsx
+++ app/assets/javascripts/components/_new_items.js.jsx
@@ -1,10 +1,17 @@
 var NewItem = React.createClass({
+  handleClick() {
+    var name = this.refs.name.value;
+    var description = this.refs.description.value;
+
+    console.log('The name value is ' + name + 'the description value is ' + description);
+  },
+
   render() {
     return (
       <div>
         <input ref='name' placeholder='Enter the name of the item' />
         <input ref='description' placeholder='Enter a description' />
-          <button>Submit</button>
+          <button onClick={this.handleClick}>Submit</button>
       </div>
     )
   }
--- test/controllers/site_controller_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SiteControllerControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get site_controller_index_url
-    assert_response :success
-  end
-
-end
